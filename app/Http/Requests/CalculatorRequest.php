92e7861a8ae3746ff37088b5693838eb142aefab
QUSAINA20 <91684341+QUSAINA20@users.noreply.github.com>
2023-03-02 20:52:20 +0200

    add calculator validation request file

---
+++ app/Http/Requests/CalculatorRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CalculatorRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'num1' => 'required|numeric',
+            'num2' => 'required_if:operation,add,subtract,multiply,divide,log|nullable|numeric',
+            'operation' => 'required|in:add,subtract,multiply,divide,log,sin,cos,tan,exp,pow'
+        ];
+    }
+}
